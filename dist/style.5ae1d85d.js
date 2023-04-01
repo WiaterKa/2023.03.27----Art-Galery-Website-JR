c79b05f5f40f6289e6e3afdc1fef8ef1a368bba6
Kamil Wiater <kamilwiater.uj@gmail.com>
2023-04-01 23:41:10 +0200

    second HTML page added + RWD implemented

--- dist/style.5ae1d85d.js
+++ dist/style.5ae1d85d.js
@@ -168,11 +168,11 @@ function reloadCSS() {
   }, 50);
 }
 module.exports = reloadCSS;
-},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"scss/main.scss":[function(require,module,exports) {
+},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"secondary page/css/style.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../assets/icon-arrow-right.svg":[["icon-arrow-right.0db7ed24.svg","assets/icon-arrow-right.svg"],"assets/icon-arrow-right.svg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../../assets/icon-arrow-left.svg":[["icon-arrow-left.a62ee8b4.svg","assets/icon-arrow-left.svg"],"assets/icon-arrow-left.svg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -342,4 +342,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/main.77bb5cfd.js.map
+//# sourceMappingURL=/style.5ae1d85d.js.map
